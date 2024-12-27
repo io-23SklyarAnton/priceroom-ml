af9474fa67b45e1d63fa73dcd24e6e6d2fb65ffd
io-23SklyarAnton <80kap.i.toshka@gmail.com>
2024-12-27 20:04:20 +0200

    fixed port

--- main.py
+++ main.py
@@ -26,4 +26,4 @@ app.include_router(api_router)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=80)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
